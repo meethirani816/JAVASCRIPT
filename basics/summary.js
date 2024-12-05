2c39e3c5a50b14c9511dff1bddbae6c2a5a975b4
MEET HIRANI <hmesmeethirani28@gmail.com>
2024-12-05 19:22:05 +0530

    change in location of file

---
+++ basics/summary.js
@@ -0,0 +1,17 @@
+// Primitive
+
+//7 Types: String, Number, boolean, null, undefined, object, symbol
+
+//Reference - Non-Primitive
+
+let heros = ["ironman", "captain america", "thor"];
+
+let myobj = {
+    name: "ironman",
+    age: 30
+}
+
+const mFunction = function(){
+    console.log("Hello, World!");
+}
+console.log(typeof mFunction);
