e57a63c0823fd7598d74dde26666c753ea2103ba
MEET HIRANI <hmesmeethirani28@gmail.com>
2024-12-05 19:46:20 +0530

    commit

--- basics/summary.js
+++ basics/summary.js
@@ -15,3 +15,24 @@ const mFunction = function(){
     console.log("Hello, World!");
 }
 console.log(typeof mFunction);
+
+// =================================================
+// Memory 
+
+// Stack (primitive) , Heap (Reference)
+
+let myYT = "AnythingYaar"
+
+let anothername = myYT
+anothername = "chai aut code"
+console.log(myYT);
+console.log(anothername);
+
+let userOne = {
+    eami: "ram@moli.com",
+    upi: "gol@okasis"
+}
+
+let useTwo = userOne
+useTwo.eami = "ram@ram.com"
+console.log(useTwo);
