dd7f23acc384eb0987250ecbf41e3532bdb1aca3
MEET HIRANI <hmesmeethirani28@gmail.com>
2024-12-27 19:08:27 +0530

    commit

--- Project/ColorChanger/javascript.js
+++ Project/ColorChanger/javascript.js
@@ -6,14 +6,42 @@ buttons.forEach(function (button) {
     button.addEventListener('click', function (e) {
     console.log(e);
     console.log(e.target);
-    if (e.target.id === 'grey') {
-        body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-        body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'purple') {
-        body.style.backgroundColor = e.target.id;
+    // if (e.target.id === 'grey') {
+    //     body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'white') {
+    //     body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'yellow') {
+    //     body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'blue') {
+    //     body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'purple') {
+    //     body.style.backgroundColor = e.target.id;
+    // }
+
+    switch (e.target) {
+        case yellow:
+            body.style.backgroundColor = e.target.id;
+            break;
+        case grey:
+            body.style.backgroundColor = e.target.id;
+            break;
+        case white:
+            body.style.backgroundColor = e.target.id;
+            break;
+        case blue:
+            body.style.backgroundColor = e.target.id;
+            break;
+        case purple:
+            body.style.backgroundColor = e.target.id;
+            break;
+    
+        default:
+            console.log('default');
+            break;
     }
     })
 
